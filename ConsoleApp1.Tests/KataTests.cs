745c58fe6e46633f0e7349b49d8d86b0ac397932
Kaiserovich <gamer281999@gmail.com>
2023-02-28 15:03:27 +0300

    Revert "TestChange"
    This reverts commit 8b28ca8c6c700908234962d3eb256a781af4c238.

--- ConsoleApp1.Tests/KataTests.cs
+++ ConsoleApp1.Tests/KataTests.cs
@@ -59,7 +59,8 @@ namespace PracticeConsoleApp.Tests
         {
             int[] a = new int[] { 121, 144, 19, 161, 19, 144, 19, 11 };
             int[] b = new int[] { 11 * 11, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19 };
-            Assert.AreEqual(true, Kata.Comp(a, b));
+            bool r = Kata.Comp(a, b); // True
+            Assert.AreEqual(true, r);
         }
         [TestMethod]
         public void FloodFillTest()
