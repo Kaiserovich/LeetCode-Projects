60e8450a10a44a6c15d6a32f0e2d88b7f5cfc8cf
Процукович Кирилл <gamer281999@gmail.com>
2023-04-26 17:22:43 +0300

    584. Find Customer Referee

--- ConsoleApp1/LeetCode.sql
+++ ConsoleApp1/LeetCode.sql
@@ -3,3 +3,6 @@ select name, population, area from World where area >= 3000000 or population >= 
 
 /*1757. Recyclable and Low Fat Products*/
 select product_id from Products where low_fats = 'Y' and recyclable = 'Y'
+
+/*584. Find Customer Referee*/
+select name from Customer where referee_id <> 2 or referee_id is null
