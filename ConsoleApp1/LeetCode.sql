7936693545bc2f0671349a80ef8d079e2e50f4d5
Процукович Кирилл <gamer281999@gmail.com>
2023-04-26 17:18:28 +0300

    1757. Recyclable and Low Fat Products

--- ConsoleApp1/LeetCode.sql
+++ ConsoleApp1/LeetCode.sql
@@ -1,2 +1,5 @@
 ﻿/*595. Big Countries*/
 select name, population, area from World where area >= 3000000 or population >= 25000000
+
+/*1757. Recyclable and Low Fat Products*/
+select product_id from Products where low_fats = 'Y' and recyclable = 'Y'
