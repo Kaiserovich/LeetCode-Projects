99293fd57b4e91ca17ddcd581c06bb62cd259550
Процукович Кирилл <gamer281999@gmail.com>
2023-04-28 11:21:15 +0300

    1795. Rearrange Products Table

--- ConsoleApp1/LeetCode.sql
+++ ConsoleApp1/LeetCode.sql
@@ -58,3 +58,16 @@ select employee_id from Employees where employee_id not in (select employee_id f
 union
 select employee_id from Salaries where employee_id not in (select employee_id from Employees)
 order by employee_id
+
+/*1795. Rearrange Products Table*/
+select product_id, 'store1' as store, store1 as price
+from Products
+where store1 is not null
+union
+select product_id, 'store2' as store, store2 as price
+from Products
+where store2 is not null
+union
+select product_id, 'store3' as store, store3 as price
+from Products
+where store3 is not null
