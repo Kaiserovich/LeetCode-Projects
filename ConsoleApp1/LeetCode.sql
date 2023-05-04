aff8910d7fe5bbecd6d53c56872c861e893f9d91
Процукович Кирилл <gamer281999@gmail.com>
2023-05-04 15:15:15 +0300

    1693. Daily Leads and Partners

--- ConsoleApp1/LeetCode.sql
+++ ConsoleApp1/LeetCode.sql
@@ -174,7 +174,12 @@ where s.sales_id not in (
 
 /*Day 7 Function*/
 /*1141. User Activity for the Past 30 Days I*/
-select activity_date as day, count(distinct user_id) as active_users
-from Activity 
-where datediff('2019-07-27',activity_date) < 30
-group by activity_date
+select activity_date as day, count(distinct user_id) as active_users 
+from Activity
+where datediff('2019-07-27', activity_date) between 0 and 29
+group by activity_date;
+
+/*1693. Daily Leads and Partners*/
+select date_id, make_name, count(distinct lead_id) as unique_leads, count(distinct partner_id ) as unique_partners 
+from DailySales
+group by date_id, make_name
