70f1d21e97bdfe7d8e3d9a6dd1fc2a7f2b26ac77
Процукович Кирилл <gamer281999@gmail.com>
2023-05-11 12:56:19 +0300

    1068. Product Sales Analysis I

--- ConsoleApp1/SQL_50.sql
+++ ConsoleApp1/SQL_50.sql
@@ -3,3 +3,8 @@
 select tweet_id from Tweets where length(content) > 15
 
 /*Basic Joins*/
+/*1378. Replace Employee ID With The Unique Identifier*/
+select unique_id, name from Employees left join EmployeeUNI using (id)
+
+/*1068. Product Sales Analysis I*/
+select product_name, year, price from Sales inner join Product using (product_id) 
