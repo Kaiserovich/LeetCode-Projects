f6964771bcb778269198c673670ce9ecf7431643
Процукович Кирилл <gamer281999@gmail.com>
2023-06-01 17:27:03 +0300

    2667. Create Hello World Function

--- ConsoleApp1/LeetCode.js
+++ ConsoleApp1/LeetCode.js
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp1
-{
-    class LeetCode
-    {
+﻿/*2703. Return Length of Arguments Passed*/
+/*1*/
+var argumentsLength = function (...args) {
+    return args.length;
+};
+/*2*/
+var argumentsLength = function (...args) {
+    let count = 0;
+    for (let i = 0; i < args.length; i++)
+        count++
+    return count;
+};
+/*2667. Create Hello World Function*/
+var createHelloWorld = function () {
+    return function () {
+        return "Hello World"
     }
-}
+};
+
