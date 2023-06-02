671e9baaa93d2cebd7ee7e072eb77125b7623632
Процукович Кирилл <gamer281999@gmail.com>
2023-06-02 16:17:34 +0300

    Binary Search

---
+++ ConsoleApp1/Algorithms.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Algorithms
+    {
+        int BinarySearch(int[] mass, int x)
+        {
+            int leftBorder = 0;
+            int rightBorder = mass.Length;
+
+            while (leftBorder <= rightBorder)
+            {
+                int center = (leftBorder + rightBorder) / 2;
+
+                if (mass[center] == x)
+                    return center;
+
+                if (mass[center] > x)
+                    rightBorder = center - 1;
+                else
+                    leftBorder = center + 1;
+            }
+            return -1;
+        }
+
+        int RecursiveBinarySearch(int[] mass, int leftBorder, int rightBorder, int x)
+        {
+            if (leftBorder > rightBorder)
+                return -1;
+
+            int center = (leftBorder + rightBorder) / 2;
+
+            if (mass[center] == x)
+                return center;
+
+            if (mass[center] > x)
+                return RecursiveBinarySearch(mass, leftBorder, center - 1, x);
+            else
+                return RecursiveBinarySearch(mass, center + 1, rightBorder, x);
+        }
+
+        int[] SelectionSort(int[] mass) {
+            for (int i = 0; i < mass.Length - 1; i++)
+            {
+                for (int j = 0; j < mass.Length; j++)
+                {
+
+                }
+            }
+
+
+            return mass;
+        }
+    }
+}
