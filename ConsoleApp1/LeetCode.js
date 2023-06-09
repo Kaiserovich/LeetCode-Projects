4091f0cb3bfd55ffbb6df2e1228d008682f750cd
Процукович Кирилл <gamer281999@gmail.com>
2023-06-09 12:12:08 +0300

    2619. Array Prototype Last

--- ConsoleApp1/LeetCode.js
+++ ConsoleApp1/LeetCode.js
@@ -21,3 +21,22 @@ var createHelloWorld = function () {
 async function sleep(millis) {
     new Promise(resolve => setTimeout(resolve, millis));
 }
+
+/*2619. Array Prototype Last*/
+/*1*/
+Array.prototype.last = function () {
+    if (this.length === 0) {
+        return -1;
+    }
+    else {
+        return this[this.length - 1];
+    }
+};
+/*2*/
+Array.prototype.last = function () {
+    return this.length ? this[this.length - 1] : -1;
+};
+/*3*/
+Array.prototype.last = function () {
+    return this[this.length - 1] ?? -1;
+};
